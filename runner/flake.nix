11164bb0483bcb21439040f8af5d93570c31e286
lyonsyonii <liam@garriga.dev>
2023-11-19 10:05:42 +0100

    Restructurated project

---
+++ runner/flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      overlays = [];
+      pkgs = import nixpkgs {
+        inherit system overlays;
+      };
+    in
+    with pkgs;
+    {
+      devShells.${system}.default = mkShell {
+        inherit nixpkgs;
+        buildInputs = with pkgs.buildPackages; [
+          rustup
+          sccache
+          mold
+        ];
+        shellHook = ''
+          rustup toolchain install stable
+          rustup toolchain install nightly-2023-10-31 -c miri rustc-codegen-cranelift-preview rust-src rust-analyzer rustfmt clippy
+        '';
+      };
+    };
+}
