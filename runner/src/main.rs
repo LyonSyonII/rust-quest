7ab2e66bdc7f04e30ac2299b5afa1e437d0bbaac
lyonsyonii <liam@garriga.dev>
2023-11-20 16:44:27 +0100

    better error handling

--- runner/src/main.rs
+++ runner/src/main.rs
@@ -1,10 +1,12 @@
 use serde::{Deserialize, Serialize};
-use tokio::io::AsyncWriteExt;
 use warp::{
     http,
     reject::{Reject, Rejection},
     Filter,
 };
+
+mod run;
+use crate::run::run;
 
 #[derive(Deserialize)]
 struct Input {
@@ -23,7 +25,7 @@ curl --request POST \
   --header 'Content-Type: application/json' \
   --header 'authorization: potato' \
   --data '{
-	"code": "fn main() { println!(\"Hello, World!\") } "
+    "code": "fn main() { println!(\"Hello, World!\") } "
 }'
 */
 
@@ -34,18 +36,29 @@ async fn main() -> Result<(), &'static str> {
     let Ok(password) = std::env::var("PASSWORD") else {
         return Err("Defining a PASSWORD environment variable is required. Please, call this program with PASSWORD=<your_password>.");
     };
-    let port = std::env::var("PORT").ok().and_then(|p| p.parse::<u16>().ok()).unwrap_or(3030);
-    let semaphore_permits = std::env::var("SEMAPHORE_PERMITS").ok().and_then(|s| s.parse::<u8>().ok()).unwrap_or(20);
-    let semaphore_wait = std::env::var("SEMAPHORE_WAIT").ok().and_then(|s| s.parse::<u16>().ok()).unwrap_or(500);
-    let kill_timeout = std::env::var("KILL_TIMEOUT").ok().and_then(|s| s.parse::<u16>().ok()).unwrap_or(500);
+    let port = std::env::var("PORT")
+        .ok()
+        .and_then(|p| p.parse::<u16>().ok())
+        .unwrap_or(3030);
+    let semaphore_permits = std::env::var("SEMAPHORE_PERMITS")
+        .ok()
+        .and_then(|s| s.parse::<u8>().ok())
+        .unwrap_or(20);
+    let semaphore_wait = std::env::var("SEMAPHORE_WAIT")
+        .ok()
+        .and_then(|s| s.parse::<u16>().ok())
+        .unwrap_or(500);
+    let kill_timeout = std::env::var("KILL_TIMEOUT")
+        .ok()
+        .and_then(|s| s.parse::<u16>().ok())
+        .unwrap_or(500);
 
     let password: &'static str = password.leak();
 
-    let cors = warp::cors()
-        .allow_origin("https://garriga.dev");
-    
+    let cors = warp::cors().allow_origin("https://garriga.dev");
+
     let semaphore = std::sync::Arc::new(tokio::sync::Semaphore::new(semaphore_permits as usize));
-    
+
     // GET /run/{code}
     let route = warp::post()
         .and(warp::path("evaluate.json"))
@@ -53,13 +66,16 @@ async fn main() -> Result<(), &'static str> {
             auth(password)
                 .and(warp::body::content_length_limit(512))
                 .and(warp::body::json())
-                .and_then(move |_, Input { code}| run(code, semaphore.clone(), semaphore_wait, kill_timeout))
+                .and_then(move |_, Input { code }| {
+                    run(code, semaphore.clone(), semaphore_wait, kill_timeout)
+                }),
         )
         .recover(handle_rejection)
         .with(cors);
-    
+
     println!("Semaphore permits: {semaphore_permits}");
     println!("Semaphore wait: {semaphore_wait}");
+    println!("Kill timeout: {kill_timeout}");
     println!("Listening on http://0.0.0.0:{}", port);
     warp::serve(route).run(([0, 0, 0, 0], port)).await;
 
@@ -78,83 +94,6 @@ fn auth(password: &'static str) -> impl Filter<Extract = ((),), Error = warp::Re
         .or_else(|_| async move { Err(warp::reject::custom(Error::NotAuthorized)) })
 }
 
-async fn run(code: impl AsRef<str>, semaphore: std::sync::Arc<tokio::sync::Semaphore>, semaphore_wait: u16, kill_timeout: u16) -> Result<impl warp::Reply, std::convert::Infallible> {
-    let code = code.as_ref();
-    let error = |e: &str| Ok(warp::reply::json(&Err::<(), _>(e)));
-    
-    let _semaphore = loop {
-        match semaphore.clone().try_acquire_owned() {
-            Ok(ok) => break ok,
-            Err(_) => tokio::time::sleep(std::time::Duration::from_millis(semaphore_wait as u64)).await,
-        }
-    };
-    
-    if code.contains("::std") {
-        return error("You can't use `::std` in your code, use the modules available with std:: instead.\nThis is done to avoid malicious activity.");
-    } else if code.contains("::core") {
-        return error("You can't use `::core` in your code.\nThis is done to avoid malicious activity.");
-    } else if code.contains("extern \"C\"") {
-        return error(
-            "You can't use `extern \"C\"` in your code.\nThis is done to avoid malicious activity.",
-        );
-    } else if code.contains("unsafe") {
-        return error(
-            "You can't use `unsafe` in your code.\nThis is done to avoid malicious activity.",
-        );
-    }
-
-    let Ok(scratch) = tempfile::Builder::new().prefix("playground").tempdir() else {
-        return error("Failed to create a temporary directory");
-    };
-    let Ok(input_file) = create_project_template(&scratch).await else {
-        return error("Failed to create the input file");
-    };
-    // println!("Writing {}", input_file.display());
-    let Ok(mut input_file) = tokio::fs::OpenOptions::new()
-        .append(true)
-        .open(input_file)
-        .await else {
-            return error("Failed to open the input file");
-        };
-
-    let Ok(_) = input_file.write_all(code.as_bytes()).await else {
-        return error("Failed to write to the input file");
-    };
-
-    let Ok(compile) = tokio::process::Command::new("cargo")
-        .args(["build"])
-        .kill_on_drop(true)
-        .current_dir(&scratch)
-        .output().await else {
-            return error("Failed to run 'cargo build'");
-        };
-    // println!("{compile:?}");
-    if !compile.status.success() {
-        return error(&String::from_utf8(compile.stderr).unwrap());
-    }
-    
-    let run = tokio::process::Command::new("cargo")
-        .arg("run")
-        .kill_on_drop(true)
-        .current_dir(&scratch)
-        .output();
-    
-    #[derive(Serialize)]
-    struct Reply {
-        result: String,
-        compile: String
-    }
-    
-    match tokio::time::timeout(std::time::Duration::from_millis(kill_timeout as u64), run).await {
-        Ok(Ok(result)) => Ok(warp::reply::json(&Reply {
-            result: String::from_utf8(result.stdout).unwrap(),
-            compile: String::from_utf8(compile.stderr).unwrap(),
-        })),
-        Ok(Err(e)) => error(&e.to_string()),
-        Err(_) => error("Timed out"),
-    }
-}
-
 async fn handle_rejection(
     err: Rejection,
 ) -> std::result::Result<impl warp::Reply, warp::Rejection> {
@@ -164,16 +103,4 @@ async fn handle_rejection(
     };
 
     Ok(warp::reply::with_status(warp::reply(), code))
-}
-
-async fn create_project_template(parent: impl AsRef<std::path::Path>) -> std::io::Result<std::path::PathBuf> {
-    let template = concat!(env!("CARGO_MANIFEST_DIR"), "/project-template/.");
-    tokio::process::Command::new("cp")
-        .arg("-r")
-        .arg(template)
-        .arg(parent.as_ref())
-        .spawn()?
-        .wait()
-        .await?;
-    Ok(parent.as_ref().join("src/main.rs"))
 }
---
+++ runner/src/run.rs
@@ -0,0 +1,150 @@
+use serde::Serialize;
+use tokio::io::AsyncWriteExt;
+
+#[derive(Serialize)]
+enum Error {
+    Std,
+    Core,
+    ExternC,
+    Unsafe,
+    TempDir,
+    InputFileCreate,
+    InputFileOpen,
+    InputFileWrite,
+    Build,
+    Compiler(String),
+    Timeout,
+    Execution(String),
+}
+
+#[derive(Serialize)]
+enum Reply {
+    Ok { stdout: String, stderr: String },
+    Err(Error),
+}
+
+impl Reply {
+    const STD: Result<Self, std::convert::Infallible> = Self::err(Error::Std);
+    const CORE: Result<Self, std::convert::Infallible> = Self::err(Error::Core);
+    const EXTERN_C: Result<Self, std::convert::Infallible> = Self::err(Error::ExternC);
+    const UNSAFE: Result<Self, std::convert::Infallible> = Self::err(Error::Unsafe);
+    const TEMP_DIR: Result<Self, std::convert::Infallible> = Self::err(Error::TempDir);
+    const INPUT_FILE_CREATE: Result<Self, std::convert::Infallible> = Self::err(Error::InputFileCreate);
+    const INPUT_FILE_OPEN: Result<Self, std::convert::Infallible> = Self::err(Error::InputFileOpen);
+    const INPUT_FILE_WRITE: Result<Self, std::convert::Infallible> = Self::err(Error::InputFileWrite);
+    const BUILD: Result<Self, std::convert::Infallible> = Self::err(Error::Build);
+    const TIMEOUT: Result<Self, std::convert::Infallible> = Self::err(Error::Timeout);
+
+    fn ok(
+        stdout: impl AsRef<[u8]>,
+        stderr: impl AsRef<[u8]>,
+    ) -> Result<Self, std::convert::Infallible> {
+        Ok(Self::Ok {
+            stdout: String::from_utf8_lossy(stdout.as_ref()).into_owned(),
+            stderr: String::from_utf8_lossy(stderr.as_ref()).into_owned(),
+        })
+    }
+    const fn err(e: Error) -> Result<Self, std::convert::Infallible> {
+        Ok(Self::Err(e))
+    }
+    fn err_compile(e: impl AsRef<[u8]>) -> Result<Self, std::convert::Infallible> {
+        Self::err(Error::Compiler(String::from_utf8_lossy(e.as_ref()).into_owned()))
+    }
+    fn err_execution(e: String) -> Result<Self, std::convert::Infallible> {
+        Self::err(Error::Execution(e))
+    }
+}
+
+impl warp::Reply for Reply {
+    fn into_response(self) -> warp::reply::Response {
+        warp::reply::json(&self).into_response()
+    }
+}
+
+pub async fn run(
+    code: impl AsRef<str>,
+    semaphore: std::sync::Arc<tokio::sync::Semaphore>,
+    semaphore_wait: u16,
+    kill_timeout: u16,
+) -> Result<impl warp::Reply, std::convert::Infallible> {
+    let code = code.as_ref();
+
+    let _semaphore = loop {
+        match semaphore.clone().try_acquire_owned() {
+            Ok(ok) => break ok,
+            Err(_) => {
+                tokio::time::sleep(std::time::Duration::from_millis(semaphore_wait as u64)).await
+            }
+        }
+    };
+
+    if code.contains("::std") {
+        return Reply::STD;
+    } else if code.contains("::core") {
+        return Reply::CORE;
+    } else if code.contains("extern \"C\"") {
+        return Reply::EXTERN_C;
+    } else if code.contains("unsafe") {
+        return Reply::UNSAFE;
+    }
+
+    let Ok(scratch) = tempfile::Builder::new().prefix("playground").tempdir() else {
+        return Reply::TEMP_DIR;
+    };
+    let Ok(input_file) = create_project_template(&scratch).await else {
+        return Reply::INPUT_FILE_CREATE;
+    };
+    // println!("Writing {}", input_file.display());
+    let Ok(mut input_file) = tokio::fs::OpenOptions::new()
+        .append(true)
+        .open(input_file)
+        .await
+    else {
+        return Reply::INPUT_FILE_OPEN;
+    };
+    let Ok(_) = input_file.write_all(code.as_bytes()).await else {
+        return Reply::INPUT_FILE_WRITE;
+    };
+    drop(input_file);
+
+    let Ok(compile) = tokio::process::Command::new("cargo")
+        .args(["build"])
+        .kill_on_drop(true)
+        .current_dir(&scratch)
+        .output()
+        .await
+    else {
+        return Reply::BUILD;
+    };
+    // println!("{compile:?}");
+    if !compile.status.success() {
+        return Reply::err_compile(&compile.stderr);
+    }
+
+    let run = tokio::process::Command::new("cargo")
+        .arg("run")
+        .kill_on_drop(true)
+        .current_dir(&scratch)
+        .output();
+    
+    // TODO: Panic does not trigger Err
+    match tokio::time::timeout(std::time::Duration::from_millis(kill_timeout as u64), run).await {
+        Ok(Ok(result)) => Reply::ok(result.stdout, result.stderr),
+        Ok(Err(e)) => Reply::err_execution(e.to_string()),
+        Err(_) => Reply::TIMEOUT,
+    }
+}
+
+async fn create_project_template(
+    parent: impl AsRef<std::path::Path>,
+) -> std::io::Result<std::path::PathBuf> {
+    let template = concat!(env!("CARGO_MANIFEST_DIR"), "/project-template/.");
+    tokio::process::Command::new("cp")
+        .arg("-r")
+        .arg(template)
+        .arg(parent.as_ref())
+        .spawn()?
+        .wait()
+        .await?;
+    Ok(parent.as_ref().join("src/main.rs"))
+}
