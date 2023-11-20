6aeb2c576d14554dc80b094c9509c87815881af4
lyonsyonii <liam@garriga.dev>
2023-11-20 11:00:54 +0100

    optimizing docker image size

--- runner/run_docker.sh
+++ runner/run_docker.sh
@@ -1,2 +1,9 @@
 sudo docker build -t lgarriga/rust-quest-runner .
-sudo docker run -p 3020:3031 -e "PASSWORD=potato" -e "PORT=3031" lgarriga/rust-quest-runner
+
+if [[ $1 == "run" ]]; then
+    sudo docker run -p 3020:3031 -e "PASSWORD=potato" -e "PORT=3031" lgarriga/rust-quest-runner
+elif [[ $1 == "push" ]]; then
+    sudo docker push lgarriga/rust-quest-runner:latest
+else
+    echo "Usage: ./run_docker.sh [run|push]"
+fi
