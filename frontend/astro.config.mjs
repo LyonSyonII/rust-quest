5c15d25c3f09d0e19506841c498ef9e70c574214
lyonsyonii <liam@garriga.dev>
2023-12-11 11:53:07 +0100

    prettier

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -55,5 +55,5 @@ export default defineConfig({
   markdown: {
     remarkPlugins: [],
     extendDefaultPlugins: true,
-  }
+  },
 });
--- frontend/src/components/Portrait/portraits.ts
+++ frontend/src/components/Portrait/portraits.ts
@@ -1,35 +1,39 @@
-import GuildGirl from "@assets/portraits/AdventurersGuildGirl.svg"; 
+import GuildGirl from "@assets/portraits/AdventurersGuildGirl.svg";
 import Grocery from "@assets/portraits/GroceryStore.svg";
 import { bgToDark } from "src/utils/colors";
 
 export const colors = {
-    purple: "#ede9fe",
-    green: "#d1fae5",
-    red: "#fecaca",
-    yellow: "#fef8c3",
-    orange: "#ffedd5",
-    blue: "#e0f2fe",
-    pink: "#fbcfe8",
-    gray: "#e5e7eb"
-}
+  purple: "#ede9fe",
+  green: "#d1fae5",
+  red: "#fecaca",
+  yellow: "#fef8c3",
+  orange: "#ffedd5",
+  blue: "#e0f2fe",
+  pink: "#fbcfe8",
+  gray: "#e5e7eb",
+};
 
 const portraits = {
-    "guild-girl": {
-        img: GuildGirl,
-        bg: colors.purple
-    },
-    "grocery": {
-        img: Grocery,
-        bg: colors.green
-    }
+  "guild-girl": {
+    img: GuildGirl,
+    bg: colors.purple,
+  },
+  grocery: {
+    img: Grocery,
+    bg: colors.green,
+  },
 };
 
 export type Portrait = keyof typeof portraits;
 
-export function portrait(portrait: Portrait): { img: ImageMetadata, bg: string, bgDark: string } {
-    const { img, bg } = portraits[portrait];
-    const bgDark = bgToDark(bg);
-    return { img, bg, bgDark };
+export function portrait(portrait: Portrait): {
+  img: ImageMetadata;
+  bg: string;
+  bgDark: string;
+} {
+  const { img, bg } = portraits[portrait];
+  const bgDark = bgToDark(bg);
+  return { img, bg, bgDark };
 }
 
 /* <Highlight bg={colors.purple}> Purple </Highlight>
