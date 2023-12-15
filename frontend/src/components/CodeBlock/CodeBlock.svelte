05034ebae9d65df9c339d57e1bc1fa2097e8ed4b
lyonsyonii <liam@garriga.dev>
2023-12-15 19:42:10 +0100

    STY: fixed padding and margins of QuestionCard and CodeBlock

--- frontend/src/components/CodeBlock/CodeBlock.svelte
+++ frontend/src/components/CodeBlock/CodeBlock.svelte
@@ -106,7 +106,7 @@
   on:clickoutside={() => (focused = false)}
 >
   {#await Promise.all( [import("svelte-codemirror-editor"), $getTheme, import("@codemirror/lang-rust")], )}
-    <pre>{value || placeholder || l.placeholder}</pre>
+    <pre class="not-content">{value || placeholder || l.placeholder}</pre>
   {:then [CodeMirror, theme, lang]}
     <CodeMirror.default
       class="not-content"
@@ -119,20 +119,21 @@
       placeholder={placeholder || l.placeholder}
     />
   {/await}
-
-  <button
-    class="not-content"
-    title="Run (Shift+Enter)"
-    disabled={running}
-    on:click={() => handleRun(true)}
-  >
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 32 32"><path fill="currentColor" d="M21 16a6 6 0 1 1-6 6a6 6 0 0 1 6-6m0-2a8 8 0 1 0 8 8a8 8 0 0 0-8-8"/><path fill="currentColor" d="M26 4H6a2 2 0 0 0-2 2v20a2 2 0 0 0 2 2h4v-2H6V12h22V6a2 2 0 0 0-2-2M6 10V6h20v4Z"/><path fill="currentColor" d="M19 19v6l5-3z"/></svg>
-  </button>
-
-  <button title="Reset code" on:click={() => (value = code)}>
-    <!-- carbon:reset -->
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 32 32"><path fill="currentColor" d="M18 28A12 12 0 1 0 6 16v6.2l-3.6-3.6L1 20l6 6l6-6l-1.4-1.4L8 22.2V16a10 10 0 1 1 10 10Z"/></svg>
-  </button>
+  
+  <div class="buttons">
+    <button
+      title="Run (Shift+Enter)"
+      disabled={running}
+      on:click={() => handleRun(true)}
+    >
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 32 32"><path fill="currentColor" d="M21 16a6 6 0 1 1-6 6a6 6 0 0 1 6-6m0-2a8 8 0 1 0 8 8a8 8 0 0 0-8-8"/><path fill="currentColor" d="M26 4H6a2 2 0 0 0-2 2v20a2 2 0 0 0 2 2h4v-2H6V12h22V6a2 2 0 0 0-2-2M6 10V6h20v4Z"/><path fill="currentColor" d="M19 19v6l5-3z"/></svg>
+    </button>
+    
+    <button title="Reset code" on:click={() => (value = code)}>
+      <!-- carbon:reset -->
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 32 32"><path fill="currentColor" d="M18 28A12 12 0 1 0 6 16v6.2l-3.6-3.6L1 20l6 6l6-6l-1.4-1.4L8 22.2V16a10 10 0 1 1 10 10Z"/></svg>
+    </button>
+  </div>
 
   {#if playgroundResponse}
     <output>
@@ -149,8 +150,8 @@
     --accent: var(--sl-color-accent);
   }
   pre {
-    height: 33.4px;
     color: gray;
+    line-height: 1.4;
   }
   .wrapper {
     display: grid;
@@ -158,15 +159,18 @@
     margin: 0rem;
 
     font-size: 0.9rem;
-    grid-template-columns: auto 30px 30px;
+    grid-template-columns: auto 60px;
+  }
+  .buttons {
+    display: flex;
+    justify-content: center;
+    gap: 0.25rem;
+    margin-left: var(--card-padding);
   }
   @media only screen and (min-width: 768px) {
     .wrapper {
       font-size: 1rem;
-      grid-template-columns: 90% auto auto;
-    }
-    button {
-      margin-left: 0.5rem;
+      grid-template-columns: 90% auto;
     }
   }
   button {
@@ -174,8 +178,8 @@
     color: var(--sl-color-white);
     cursor: pointer;
     border: 0px;
-    margin-left: 0rem;
-    padding-bottom: 0px;
+    margin: 0rem;
+    padding: 0px;
     height: fit-content;
     transition: opacity 0.25s ease;
   }
