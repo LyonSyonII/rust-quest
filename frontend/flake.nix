3b8fd00e9176dccef004cac026689cf2f2875f5b
LyonSyonII <liam.garriga@tutanota.com>
2024-01-17 16:50:42 +0100

    CHO: added flake.nix to project

---
+++ frontend/flake.nix
@@ -0,0 +1,16 @@
+{
+  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; };
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux.pkgs;
+    in
+    with pkgs;
+    {
+      devShells.x86_64-linux.default = mkShell {
+        buildInputs = [
+          nodejs_21
+          corepack_21
+        ];
+      };
+    };
+}
