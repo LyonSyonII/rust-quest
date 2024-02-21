3d6e27ef7c84a6223e490659bb0143b9b4900107
lyonsyonii <liam@garriga.dev>
2024-02-21 20:55:37 +0100

    feat: added dockerfile to feedbacker; migrated to axum

--- feedbacker/flake.nix
+++ feedbacker/flake.nix
@@ -34,6 +34,7 @@
           valgrind
           mold
           cargo-watch
+          docker
         ];
       };
     };
--- feedbacker/src/main.rs
+++ feedbacker/src/main.rs
@@ -1,19 +1,23 @@
+use axum::{
+    body::Body,
+    response::Json,
+    routing::{get, post},
+    Router,
+};
+use serde_json::{json, Value};
 use std::fmt::Write;
-
-use actix_web::{get, http, post, web, App, HttpResponse, HttpServer, Responder};
-
 
 #[derive(serde::Deserialize, Debug, Clone)]
 struct Feedback {
     id: String,
     score: u8,
-    review: String
+    review: String,
 }
 
-#[derive(serde::Deserialize, Debug, Clone)]
+#[derive(Debug, Clone, serde::Deserialize, serde::Serialize)]
 struct Config {
     port: u16,
-    allowed_origins: Vec<String>
+    allowed_origins: Vec<String>,
 }
 
 impl Default for Config {
@@ -25,35 +29,46 @@ impl Default for Config {
     }
 }
 
-#[post("/")]
-async fn feedback(web::Json(feedback): web::Json<Feedback>) -> impl Responder {
-    println!("{}", feedback);
-    HttpResponse::Ok().body("Thanks for your feedback!")
+/// GET /
+async fn index() -> &'static str {
+    "Waiting for feedback..."
 }
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    let config = figment::Figment::new().merge(
-        figment::providers::Env::prefixed("")
-    ).extract::<Config>().unwrap();
-        
-    let cors = actix_cors::Cors::default()
-        .allowed_origin_fn(|origin, _| {
-            config.allowed_origins.iter().any(|a| a.as_bytes() == origin.as_bytes())
-        })
-        .allowed_methods(["POST", "OPTIONS"])
-        .allowed_header(http::header::CONTENT_TYPE);
+/// POST /
+async fn feedback(Json(feedback): Json<Feedback>) -> &'static str {
+    println!("{}", feedback);
+    "Thank you for your feedback!"
+}
 
-    let app = App::new().service(feedback).wrap(cors);
+#[tokio::main]
+async fn main() {
+    let config =
+        figment::Figment::from(figment::providers::Serialized::defaults(Config::default()))
+            .merge(figment::providers::Env::prefixed(""))
+            .extract::<Config>()
+            .unwrap();
 
-    HttpServer::new(move || {
-        
+    let cors = tower_http::cors::CorsLayer::new().allow_origin(
+        config
+            .allowed_origins
+            .into_iter()
+            .map(|o| o.parse::<http::HeaderValue>().unwrap())
+            .collect::<Vec<_>>(),
+    );
 
-        app
-    })
-    .bind(("0.0.0.0", config.port))?
-    .run()
-    .await
+    let app = axum::Router::new()
+        .route("/", post(feedback))
+        .route("/", get(index))
+        .layer(cors);
+
+    let listener = tokio::net::TcpListener::bind(("0.0.0.0", config.port))
+        .await
+        .unwrap();
+
+    let ip = local_ip_address::local_ip().unwrap();
+    println!("Listening on: http://{ip:?}:{}", config.port);
+
+    axum::serve(listener, app).await.unwrap();
 }
 
 #[derive(serde::Serialize, serde::Deserialize, Debug, Clone)]
@@ -61,7 +76,7 @@ struct Log<'a> {
     date: time::OffsetDateTime,
     id: &'a str,
     score: u8,
-    review: &'a str
+    review: &'a str,
 }
 
 impl<'a> From<&'a Feedback> for Log<'a> {
@@ -70,7 +85,7 @@ impl<'a> From<&'a Feedback> for Log<'a> {
             date: time::OffsetDateTime::now_utc(),
             id: &value.id,
             score: value.score,
-            review: &value.review
+            review: &value.review,
         }
     }
 }
