97f91d6e2489a264e2a0dfd17d7a26b930087361
LyonSyonII <liam.garriga@tutanota.com>
2024-02-24 15:29:56 +0100

    fix(4-1): improved error detection and messages

--- frontend/src/validation/4-1.ts
+++ frontend/src/validation/4-1.ts
@@ -7,11 +7,12 @@ function validator(value: string, test: (regex: RegExp, ignoreWhitespace?: boole
       || !test(/let name = ".+";/) && "[name] Something is wrong with your name, click the reset button if you changed it by mistake!"
       || test(/let surname = (\?|"");/) && "[surname] Fill in your surname!"
       || !surname.includes('"') && "[surname] Something is wrong with your surname.\nLook closely to how 'name' is written!"
-      || (age.includes("?") || age.trim().length == 0) && "[age] Fill in your age!"
+      || (age.includes("?") || age.trim().length === 0) && "[age] Fill in your age!"
+      || isNaN(Number(age)) && "[age] Age must be a number!"
       || Number(age) < 0 && "[age] Age can't be negative, can it? 😉"
       || Number(age) < 15 && "[age] You must be at least 15 years old to go adventuring!"
       || value.includes("?") && replace
-      || !test(/^let name = ".*";\nlet surname = (\?|".*");\nlet mut age = (\?|\d+);$/) && "Seems like you've messed up the code, click the 'Reset' button to return it back to its original state."
+      || !test(/^let name = ".*";\nlet surname = ".*";\nlet mut age = \d+;$/) && "Seems like you've messed up the code, click the 'Reset' button to return it back to its original state."
       || undefined;
 }
 
