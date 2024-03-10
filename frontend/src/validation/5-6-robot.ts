07f15f5a505b8bc2bc0c934debd3c2ec7a673b43
LyonSyonII <liam.garriga@tutanota.com>
2024-03-10 12:19:42 +0100

    feat: improved exercise 5-6 with better validation and added a Hint and Note

--- frontend/src/validation/5-6-robot.ts
+++ frontend/src/validation/5-6-robot.ts
@@ -1,6 +1,6 @@
 import { Functions, type RobotGameProps } from "@components/RobotGame/RobotGame";
 import { codeMess, replace } from "./CodeQuestion";
-import { createRegExp, exactly, maybe, word } from "magic-regexp";
+import { createRegExp, exactly, maybe, not, oneOrMore, word } from "magic-regexp";
 import { _, end, line, start } from "./regex";
 
 export default {
@@ -24,23 +24,30 @@ export default {
 } as RobotGameProps;
 
 function validator(value: string): string | undefined {
+  const instructions = exactly(_, oneOrMore(not.whitespace)).times.any();
   const regex = createRegExp(
     start, _,
     line, _,
-    "if", _, exactly("?").or(word, "()", maybe(";")).as("first"), _, "{", _, line, _, line, _, line.as("extra1"), _, "}", _,
-    "if", _, exactly("?").or(word, "()", maybe(";")).as("second"), _, "{", _, line, _, line, _, line.as("extra2"), _, "}", _,
+    "if", _, exactly("?").or(word, "()", maybe(";")).as("first"), _, "{", instructions.as("inst1"), _, "}", _,
+    "if", _, exactly("?").or(word, "()", maybe(";")).as("second"), _, "{", instructions.as("inst2"), _, "}", _,
     end
   );
   const matches = value.match(regex);
   if (!matches) return codeMess;
-
-  const { first, second, extra1, extra2 } = matches.groups;
+  
+  const { first, second, inst1 = "", inst2 = "" } = matches.groups;
   if (!first || !second) return codeMess;
 
   const wrong = "Look closely at the instructions, you don't need a semicolon!";
-
+  
+  // slice to remove last semicolon
+  const instCount = Math.max(
+    inst1.trim().slice(0, -1).split(";").length, 
+    inst2.trim().slice(0, -1).split(";").length
+  );
+  
   return value.includes("?") && replace
-    || (extra1 || extra2) && "You don't need more than two lines in each condition!"
+    || instCount > 2 && "You don't need more than two instructions in each condition!"
     || first.includes(";") && `[first condition] ${wrong}`
     || second.includes(";") && `[second condition] ${wrong}`
     || undefined
