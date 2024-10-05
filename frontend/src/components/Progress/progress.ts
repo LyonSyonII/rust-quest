be7d7aed35b500359ee5caf34c835a727894bb3f
lyonsyonii <liam@garriga.dev>
2024-10-05 11:49:54 +0200

    fix: progress bar not updating on page load

--- frontend/src/components/Progress/progress.ts
+++ frontend/src/components/Progress/progress.ts
@@ -13,29 +13,29 @@ export class Progress extends HTMLElement {
     const id: string = progress.id;
     const total: number = progress.max;
 
-    subscribe(id, async (checkpoint) => {
-      const value = checkpoint.size < total ? checkpoint.size : total;
-      label.innerText = `${value} / ${total}`;
-      progress.value = value;
-      if (value !== total) {
-        return;
-      }
-      const completedId = `${id}-completed`;
-      if (launchConfetti && !checkpoint.has(completedId)) {
-        confetti();
-      }
-      add(completedId);
-    });
+    subscribe(
+      id,
+      async (checkpoint) => {
+        const value = checkpoint.size < total ? checkpoint.size : total;
+        label.innerText = `${value} / ${total}`;
+        progress.value = value;
+        if (value !== total) {
+          return;
+        }
+        const completedId = `${id}-completed`;
+        if (launchConfetti && !checkpoint.has(completedId)) {
+          confetti();
+        }
+        add(completedId);
+      },
+      true,
+    );
 
     $("button#import", this).addEventListener("click", () => importData());
-    $("button#export", this).addEventListener("click", async () =>
-      exportData(),
-    );
-
+    $("button#export", this).addEventListener("click", () => exportData());
+    
     const feedback = $("dialog", this);
-    $("button#feedback", this).addEventListener("click", () =>
-      feedback.showModal(),
-    );
+    $("button#feedback", this).addEventListener("click", () => feedback.showModal());
 
     if (import.meta.env.DEV) {
       $("button#reset", this).addEventListener("click", async () => {
